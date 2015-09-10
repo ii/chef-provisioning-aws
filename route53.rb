edd544afa7a884745c2668196d68d359e8b28117
Chris Doherty <cdoherty@chef.io>
2015-09-10 16:35:59 -0700

    route53.rb: paring down.

--- route53.rb
+++ route53.rb
@@ -6,37 +6,24 @@ with_driver 'aws::us-east-1'
 
 zone_name = "cdoherty-aws-development-delete-me.com."
 
-aws_route53_hosted_zone zone_name do
-  action :destroy
-end
+# aws_route53_hosted_zone zone_name do
+#   action :destroy
+# end
+
+require 'securerandom'
 
 rs = [ # required
     {
       action: "UPSERT", # required, accepts CREATE, DELETE, UPSERT
       resource_record_set: { # required
-        name: "target.example.com.", # required
+        name: "some-api-host.#{zone_name}", # required
         type: "CNAME", # required, accepts SOA, A, TXT, NS, CNAME, MX, PTR, SRV, SPF, AAAA
-        set_identifier: "ResourceRecordSetIdentifier",
-        weight: 1,
-        region: "us-east-1", # accepts us-east-1, us-west-1, us-west-2, eu-west-1, eu-central-1, ap-southeast-1, ap-southeast-2, ap-northeast-1, sa-east-1, cn-north-1
-        geo_location: {
-          continent_code: "GeoLocationContinentCode",
-          country_code: "GeoLocationCountryCode",
-          subdivision_code: "GeoLocationSubdivisionCode",
-        },
-        failover: "PRIMARY", # accepts PRIMARY, SECONDARY
         ttl: 1,
         resource_records: [
           {
-            value: "RData", # required
+            value: "some-other-host.example.com.", # required
           },
         ],
-        alias_target: {
-          hosted_zone_id: "ResourceId", # required
-          dns_name: "DNSName", # required
-          evaluate_target_health: true, # required
-        },
-        health_check_id: "HealthCheckId",
       },
     },
   ]

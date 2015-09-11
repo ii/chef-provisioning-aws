96f68324b13359d1bb61f0457887f084517a745f
Chris Doherty <cdoherty@chef.io>
2015-09-11 11:49:06 -0700

    Trying out new route53 DSL.

--- route53.rb
+++ route53.rb
@@ -28,11 +28,37 @@ rs = [ # required
     },
   ]
 
-aws_route53_hosted_zone zone_name do
-  action :create
-  comment "Contact Chris Doherty <cdoherty@chef.io> with questions about this. "
-  record_sets rs
+# if aws_route53_recordset only supports :nothing, and in_aws_route53_hosted_zone runs at
+# compile-time, I think the latter can stick the RecordSets in a data structure that 
+# aws_route53_hosted_zone can access at converge time?
+
+aws_route53_hosted_zone "home.example.com" do
+  action :nothing
+  comment "The zone stands alone."
 end
+
+in_aws_route53_hosted_zone "home.example.com" do |zone_name|
+  aws_route53_recordset "something.#{zone_name} CNAME" do
+    rr_name "some-api-host.example.com"
+    type "CNAME"
+    ttl 3600
+    resource_records [{ value: "some-other-host.example.com."}]
+    hosted_zone_name zone_name
+  end
+
+  aws_route53_recordset "something.#{zone_name} A" do
+    rr_name "some-api-host.example.com"
+    type "A"
+    ttl 3600
+    resource_records [
+      { value: "141.222.2.2",   weight: 10 },
+      { value: "192.168.10.89", weight: 20 },
+    ]
+    hosted_zone_name zone_name
+  end
+end
+
+  # comment "Contact Chris Doherty <cdoherty@chef.io> with questions about this. "
 
 # aws_route53_hosted_zone zone_name do
 #   action :create

33737c1f8e488a6e70c02140e2af9b5f11b564a6
Chris Doherty <cdoherty@chef.io>
2015-09-11 16:45:47 -0700

    route53.rb: working on recordsets.

--- route53.rb
+++ route53.rb
@@ -32,33 +32,36 @@ rs = [ # required
 # compile-time, I think the latter can stick the RecordSets in a data structure that 
 # aws_route53_hosted_zone can access at converge time?
 
-aws_route53_hosted_zone "home.example.com" do
+log "first log resource" do
   action :nothing
+end
+
+aws_route53_hosted_zone "home.example.com" do
+  action :create
   comment "The zone stands alone."
+  record_sets {
+    aws_route53_record_set "some-hostname CNAME" do
+      rr_name "some-api-host.example.com"
+      type "CNAME"
+      ttl 3600
+      resource_records [{ value: "some-other-host.example.com."}]
+    end
+
+    aws_route53_record_set "something A" do
+      rr_name "some-api-host.example.com"
+      type "A"
+      ttl 3600
+      resource_records [
+        { value: "141.222.2.2",   weight: 10 },
+        { value: "192.168.10.89", weight: 20 },
+      ]
+      hosted_zone_name zone_name
+    end
+  }
 end
 
-in_aws_route53_hosted_zone "home.example.com" do |zone_name|
-  aws_route53_recordset "something.#{zone_name} CNAME" do
-    rr_name "some-api-host.example.com"
-    type "CNAME"
-    ttl 3600
-    resource_records [{ value: "some-other-host.example.com."}]
-    hosted_zone_name zone_name
-  end
+log "second log resource"
 
-  aws_route53_recordset "something.#{zone_name} A" do
-    rr_name "some-api-host.example.com"
-    type "A"
-    ttl 3600
-    resource_records [
-      { value: "141.222.2.2",   weight: 10 },
-      { value: "192.168.10.89", weight: 20 },
-    ]
-    hosted_zone_name zone_name
-  end
-end
-
-  # comment "Contact Chris Doherty <cdoherty@chef.io> with questions about this. "
 
 # aws_route53_hosted_zone zone_name do
 #   action :create

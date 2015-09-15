79103b693f49089256914d260c185f6d80c775bf
Chris Doherty <cdoherty@chef.io>
2015-09-15 15:24:49 -0700

    checkpoint while using tests

--- route53.rb
+++ route53.rb
@@ -6,9 +6,9 @@ with_driver 'aws::us-east-1'
 
 zone_name = "cdoherty-aws-development-delete-me.com"
 
-# aws_route53_hosted_zone zone_name do
-#   action :create
-# end
+aws_route53_hosted_zone zone_name do
+  action :purge
+end
 
 # if aws_route53_recordset only supports :nothing, and in_aws_route53_hosted_zone runs at
 # compile-time, I think the latter can stick the RecordSets in a data structure that 
@@ -18,28 +18,33 @@ zone_name = "cdoherty-aws-development-delete-me.com"
 #   action :nothing
 # end
 
-aws_route53_hosted_zone "home.example.com" do
-  action :purge
+aws_route53_hosted_zone zone_name do
+  action :create
   comment "The zone stands alone."
-  # record_sets {
-  #   aws_route53_record_set "some-hostname CNAME" do
-  #     rr_name "some-api-host.example.com"
-  #     type "CNAME"
-  #     ttl 3600
-  #     resource_records [{ value: "some-other-host.example.com."}]
-  #   end
+  record_sets {
+    aws_route53_record_set "some-hostname CNAME" do
+      rr_name "some-api-host.example.com"
+      type "CNAME"
+      ttl 3600
+      resource_records [{ value: "some-other-host.example.com."}]
+    end
 
-  #   # aws_route53_record_set "something A" do
-  #   #   rr_name "some-api-host.example.com"
-  #   #   type "A"
-  #   #   ttl 3600
-  #   #   resource_records [
-  #   #     { value: "141.222.2.2",   weight: 10 },
-  #   #     { value: "192.168.10.89", weight: 20 },
-  #   #   ]
-  #   #   hosted_zone_name zone_name
-  #   # end
-  # }
+    aws_route53_record_set "something A" do
+      rr_name "some-api-host.#{zone_name}"
+      type "A"
+      ttl 3600
+      resource_records [
+        { value: "141.222.2.2"   },
+        { value: "192.168.10.89" },
+      ]
+      hosted_zone_name zone_name
+    end
+  }
 end
+
+# aws_route53_hosted_zone zone_name do
+#   action :purge
+# end
+
 
 # log "second log resource"
